3dffd2734e3db869775391f89b6a35761000fb6e
baltsers <haipeng.cai@wsu.edu>
2023-04-22 20:13:55 +0000

    add a vul in operator.mod

---
+++ Python3.9.15/operator.mod/1#operator#mod.py
@@ -0,0 +1,14 @@
+from operator import *
+import operator
+
+
+def demoFunc(arg1,arg2):
+    try:
+        ret = operator.mod(arg1, arg2)
+    except (AssertionError, AttributeError, ImportError, LookupError, OSError, TypeError, ValueError) as e:
+        pass
+
+a= "155%7000000000000ret00000015082063?0000000000000000p1155900000005082063303862299307%+4"
+b= "!@#$%^&*9523"
+with open('/dev/null', 'r'):
+    demoFunc(a,b)
